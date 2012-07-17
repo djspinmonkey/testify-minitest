baa52c47ea78a338a439a7b2ecaef1f6948b6c65
John Hyland <jhyland@damballa.com>
2012-07-17 17:08:57 -0400

    Lots of mucking around, trying to get minitest to run.

--- lib/minitest_adaptor.rb
+++ lib/minitest_adaptor.rb
@@ -1,37 +1,19 @@
 module Testify
-  # A Testify Framework to run MiniTest tests.  Or put another way, a
-  # MiniTest::Unit runner that runs as a Testify framework.
+  # A MiniTest::Unit runner instrumented to work with Testify::MiniTestAdaptor
   #
-  # This is tied a more closely to the MiniTest::Unit implementation than is
-  # ideal. Unfortunately, I don't see a good way to insert hooks around running
-  # each individual test, so I've had to re-implement some of the surrounding
-  # logic.
-  #
-  class MiniTestAdaptor < MiniTest::Unit
-    include Testify::Framework
-
-    aka :minitest
-    statuses :passed, :failed, :error, :skipped
-    file_pattern '*_test.rb'
-
+  class Unit < MiniTest::Unit
     attr_accessor :results
 
-    def call( env )
-      self.results = []
-      self._run
-      results
-    end
-
-    ###### Below this line we are overriding MiniTest::Unit methods. ######
- 
     def initialize
       super
+
+      @results = []
 
       # squash output
       null_output = Object.new
       def null_output.puts(*_);  end
       def null_output.print(*_); end
-      MiniTestAdaptor.output = null_output
+      self.class.output = null_output
     end
 
     # Run one MiniTest::Unit::TestCase's worth of tests
@@ -65,21 +47,22 @@ module Testify
     def puke klass, meth, e
       case e
       when MiniTest::Skip then
-        @results << Testify::TestResult(
+        @results << Testify::TestResult.new(
           :status => :skipped,
           :message => "Skipped:\n#{meth}(#{klass}) [#{location e}]:\n#{e.message}\n"
           # TODO: parse file and line out of location
         )
       when MiniTest::Assertion then
-        @results << Testify::TestResult(
+        @results << Testify::TestResult.new(
           :status => :failed,
           :message => "Failure:\n#{meth}(#{klass}) [#{location e}]:\n#{e.message}\n"
           # TODO: parse file and line out of location
         )
       else
-        @results << Testify::TestResult(
+        @results << Testify::TestResult.new(
           :status => :error,
-          :message => "Error:\n#{meth}(#{klass}):\n#{e.class}: #{e.message}\n    #{bt}\n"
+          :message => 'ERROR OF SOME SORT I DUNNO',
+          #:message => "Error:\n#{meth}(#{klass}):\n#{e.class}: #{e.message}\n    #{bt}\n"
           # TODO: keep the backtrace: bt = MiniTest::filter_backtrace(e.backtrace).join "\n    "
           # TODO: parse file and line out of location
         )
@@ -87,6 +70,23 @@ module Testify
 
       return 'unused return value'
     end
+  end
 
+  # A Testify Framework to run MiniTest tests.
+  #
+  class MiniTestAdaptor
+    include Testify::Framework
+
+    aka :minitest
+    statuses :passed, :failed, :error, :skipped
+    file_pattern '*_test.rb'
+
+    def call( env )
+      runner = MiniTest::Unit.runner = Testify::Unit.new
+      files(env).each { |f| require f }
+      runner.run
+
+      runner.results
+    end
   end
 end
